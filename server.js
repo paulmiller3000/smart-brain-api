cf695e4d73b89581333c4b6e933f9fa9a140d119
Paul Miller <paul@battlestardigital.com>
2019-07-12 20:59:42 -0400

    Small cleanup

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
-
 const image = require('./controllers/image');
 const profile = require('./controllers/profile');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
+const PORT = process.env.PORT;
 
 const db = knex({
 	client: 'postgres',
@@ -25,13 +25,12 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => { res.send(database.users) });
-
 app.put('/image', (req, res) => { image.handleImage(db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(db) });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 app.post('/signin', signin.handleSignin(db, bcrypt));
 
-app.listen(process.env.PORT, () => {
-	console.log(`Application is running on port ${process.env.PORT}.`);
+app.listen(PORT, () => {
+	console.log(`Application is running on port ${PORT}.`);
 })
